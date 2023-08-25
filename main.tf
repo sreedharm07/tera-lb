93e015286d99739044b4b09acdb41413cb792866
sreedhar <sreedhar70755@gmail.com>
2023-08-26 05:15:38 +0530

    tf

--- main.tf
+++ main.tf
@@ -29,3 +29,19 @@ tags = merge(local.tags,{Name= "${var.env}-sg"})
     ipv6_cidr_blocks = ["::/0"]
   }
 }
+
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "Fixed response content"
+      status_code  = "200"
+    }
+  }
+}
